35ae78f1d0739a59c0937369e3e35f93dfeca7c1
Jovelasirot <jovelasirot@gmail.com>
2024-05-03 15:22:48 +0200

    minor form fixes

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,32 +1,80 @@
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 
 const SignUpForm = () => {
   return (
     <Container fluid className="signImgBg">
       <Row className="align-itmes-center">
         <Col>
-          <Card id="signUpRectangle" className="py-5 px-5 ">
-            <i className="bi bi-arrow-left-short"></i>
-            <Link to="/" className="text-decoration-none">
-              <i className="bi bi bi-arrow-left-short"></i>
+          <Card id="signUpRectangle" className="py-5  px-5">
+            <Link to="/" className="text-decoration-none ">
+              <i className="bi bi bi-arrow-left fs-3"></i>
             </Link>
-            <Card.Body className="d-flex flex-column  justify-content-center">
-              <Card.Title className="fs-3 fw-light">
+            <Card.Body className="d-flex flex-column">
+              <Card.Title className="fs-3 fw-light border-bottom">
                 <span className="fw-bold text-primary"> Create </span>a new
                 account
+                <span className="invisble">
+                  ‎ ‎ ‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎
+                  ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎‎ ‎ ‎
+                </span>
               </Card.Title>
-
+              <Card.Text className="mt-2">
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="First name"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Surname</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Surname"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Username"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Password"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Confir password"
+                      className="w-100"
+                    />
+                  </Form.Group>
+                </Form>
+              </Card.Text>
               <Link to="/register" className="w-100 mt-3 ">
                 <Button variant="primary w-100">Sign-up</Button>
               </Link>
-
-              <Card.Text className="mt-2 text-light">
-                Already have a register account, you can{" "}
-                <span className="text-decoration-underline text-primary ">
-                  Log-In
-                </span>
-              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
