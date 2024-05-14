232ced2f295dfd660faa065461c51b86b4632968
Jovelasirot <jovelasirot@gmail.com>
2024-05-14 11:27:48 +0200

    log out added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import MyNabBar from "./components/MyNabBar";
 import SignUp from "./components/SignUp";
 import SignUpForm from "./components/SignUpForm";
 import SignIn from "./components/SignIn";
 import HomePage from "./components/HomePage";
 import Profile from "./components/Profile";
+import MyNavBar from "./components/MyNavBar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const token = localStorage.getItem("token");
   return (
     <>
       <BrowserRouter>
-        <MyNabBar />
+        <MyNavBar />
         <Routes>
           <Route
             path="/"
@@ -20,8 +21,22 @@ function App() {
           />
           <Route path="/register" element={<SignUpForm />} />
           <Route path="/login" element={<SignIn />} />
-          <Route path="/homepage" element={<HomePage />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/homepage"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
